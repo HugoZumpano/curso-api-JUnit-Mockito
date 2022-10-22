87fb6b9c03dc36b9b995496d28d229d46fb0ed4d
Hugo Zumpano Gomes <hugo_zumpano@hotmail.com>
2022-10-22 13:53:45 -0300

    Class user

---
+++ src/main/java/br/com/hugo/api/domain/User.java
@@ -0,0 +1,25 @@
+package br.com.hugo.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer  id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
