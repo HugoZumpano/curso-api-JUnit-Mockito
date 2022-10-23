e521f5722d8c10561b6fc63bba0e0fe97dfecd9f
Hugo Zumpano Gomes <hugo_zumpano@hotmail.com>
2022-10-23 00:35:47 -0300

    interface user repository

---
+++ src/main/java/br/com/hugo/api/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package br.com.hugo.api.repositories;
+
+import br.com.hugo.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+    @Repository
+    public interface UserRepository extends JpaRepository<User, Integer>{
+
+
+    }
+
+
