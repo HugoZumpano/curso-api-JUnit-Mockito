8c0464fda20c7721905ba9c25e3d2a1942bdf1bf
Hugo Zumpano Gomes <hugo_zumpano@hotmail.com>
2022-10-30 11:45:02 -0300

    endpoint create

--- src/main/java/br/com/hugo/api/services/UserService.java
+++ src/main/java/br/com/hugo/api/services/UserService.java
@@ -1,12 +1,13 @@
 package br.com.hugo.api.services;
 
 import br.com.hugo.api.domain.User;
+import br.com.hugo.api.domain.dto.UserDTO;
 
 import java.util.List;
 
 public interface UserService {
 
     User findById(Integer id);
-
     List<User> findAll();
+    User create(UserDTO obj);
 }
