1178291fa1744cd396142b8cf48b0e2fbd30f361
Hugo Zumpano Gomes <hugo_zumpano@hotmail.com>
2022-10-30 23:49:40 -0300

    teste para classe principal

--- src/test/java/br/com/hugo/api/ApiApplicationTests.java
+++ src/test/java/br/com/hugo/api/ApiApplicationTests.java
@@ -1,13 +1,16 @@
 package br.com.hugo.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.TargetSource;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiApplicationTests {
 
     @Test
-    void contextLoads() {
+    void main() {
+        ApiApplication.main(new String[] {});
     }
 
 }
