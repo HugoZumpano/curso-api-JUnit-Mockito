efc90d8a2c7d03861146da4110d87f8dcfe78bc9
Hugo Zumpano Gomes <hugo_zumpano@hotmail.com>
2022-11-05 12:00:40 -0300

    teste para classe Userservice

---
+++ src/test/java/br/com/hugo/api/resources/UserResourceTest.java
@@ -0,0 +1,38 @@
+package br.com.hugo.api.resources;
+
+import br.com.hugo.api.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class UserResourceTest {
+
+    private
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
