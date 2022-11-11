7d308915be19a576b236b361750651859a02b42e
Hugo Zumpano Gomes <hugo_zumpano@hotmail.com>
2022-11-10 22:27:06 -0300

    classe de teste para user resource iniciada

--- src/test/java/br/com/hugo/api/resources/UserResourceTest.java
+++ src/test/java/br/com/hugo/api/resources/UserResourceTest.java
@@ -1,19 +1,38 @@
 package br.com.hugo.api.resources;
 
-import br.com.hugo.api.services.UserService;
+import br.com.hugo.api.config.ModelMappperConfig;
+import br.com.hugo.api.domain.User;
+import br.com.hugo.api.domain.dto.UserDTO;
+import br.com.hugo.api.services.impl.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class UserResourceTest {
+    private User user;
 
-    private
+    private UserDTO userDTO;
 
+
+    @InjectMocks
+    private UserResource resource;
+
+    private UserServiceImpl service;
+
+    private ModelMapper mapper;
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
     }
 
     @Test
@@ -34,5 +53,12 @@ class UserResourceTest {
 
     @Test
     void delete() {
+    }
+
+    private void startUser() {
+
+        user = new User(1, "hugo", "email@hotmail", "123");
+        userDTO = new UserDTO(1, "hugo", "email@hotmail", "123");
+
     }
 }
